ecd850652765e82db16fd42f6d25c95e099ad2a1
prince-ravi-leow <princeravileow@outlook.com>
2023-08-21 17:24:52 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# `snippet_make.py`
+
+Easily create VS Code Snippet by copy-pasting into the command line, or loading the desired text from a text file.
+
+See [official docs](https://code.visualstudio.com/docs/editor/userdefinedsnippets), to make your snippet executable.
+
+# Usage
+```text
+$ python3 snippet_make.py --help
+usage: snippet_make.py [-h] [--text_file BODY] [--paste PASTE] --title TITLE --prefix PREFIX [--desc DESC]
+
+
+options:
+  -h, --help        show this help message and exit
+  --text_file BODY  Load code stored in text file
+  --paste PASTE     Paste code directly into console
+  --title TITLE     Name of snippet in JSON
+  --prefix PREFIX   Shortcut to call snippet
+  --desc DESC       Descriptive description of your snippet (Optional)
+```
+
+# Example
+**Example snippet:**
+```python
+#!/usr/bin/env python3
+```
+**Input:**
+```shell
+$ python3 snippet_make.py --text_file test/test_py_shebang.txt --title "Python shebang" --prefix "pyshebang" 
+```
+**Output:**
+```json
+"Python shebang": {
+    "prefix": "pyshebang",
+    "body": [
+        "#!/usr/bin/env python3"
+    ],
+    "description": ""
+}
+```
+
+Credit to **[@xiaket](https://stackoverflow.com/users/411662/xiaket) et al.** on this [StackOverflow post](https://stackoverflow.com/questions/30239092/how-to-get-multiline-input-from-the-user), for implementation of multi-line text input in Python. 🙏
---
+++ snippet_make.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+
+import sys
+import json
+from argparse import ArgumentParser
+
+### PARSE ARGUMENTS
+parser = ArgumentParser(description="Convert text into VS Code Snippet")
+
+parser.add_argument("--text_file", 
+                    action="store", 
+                    dest="body", 
+                    type=str, 
+                    help="Load code stored in text file")
+parser.add_argument("--paste", 
+                    action="store", 
+                    dest="paste", 
+                    type=bool, 
+                    help="Paste code directly into console")
+parser.add_argument("--title", 
+                    action="store", 
+                    dest="title", 
+                    type=str, 
+                    help="Name of snippet in JSON", 
+                    required=True)
+parser.add_argument("--prefix", 
+                    action="store", 
+                    dest="prefix", 
+                    type=str, 
+                    help="Shortcut to call snippet", 
+                    required=True)
+parser.add_argument("--desc", 
+                    action="store", 
+                    dest="desc", 
+                    type=str, 
+                    help="Descriptive description of your snippet (Optional)")
+
+if len(sys.argv) < 2:
+    parser.print_help()
+    sys.exit(1)
+
+args = parser.parse_args()
+paste = args.paste
+body = args.body
+title = args.title
+prefix = args.prefix
+desc = args.desc
+
+### MAIN BODY
+# Load snippet body from text file
+if body:
+    snippet_body = []
+    with open(body, "rt") as input_file:
+    		for line in input_file:
+    			snippet_body.append(line)
+                        
+if not desc:
+    desc = ""
+			
+# Load snippet body from clipboard 
+if paste:
+    print("Enter/Paste your content, hit RETURN, followed by Ctrl-D (Ctrl-Z on Windows) to accept input.\n")
+    snippet_body = []
+    while True:
+        try:
+            line = input()
+        except EOFError:
+            break
+        snippet_body.append(line)
+
+# Construct VS Code Snippet JSON object
+snippet_object = {
+    "prefix": prefix,
+    "body": snippet_body,
+    "description": desc
+}
+snippet_json = f'"{title}": ' + json.dumps(snippet_object, indent = 4)
+
+# Print output to console
+print(f"\n{snippet_json}\n")
